25c04ad23f9b1dbd1160a201aeefebea32253829
Naaikho <natsun_prod@outlook.fr>
2022-10-01 20:26:30 +0200

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import sys, os
+import time
 
-# path = sys.argv[1]
-path = sys.path[0]
-# file = sys.argv[2]
-file = "app.log"
+try:
+    PATH = sys.argv[1]
+    file = sys.argv[2]
+except IndexError:
+    print("File error: No file specified")
+    exit()
 
-def path(p:str, *args):
-    for elm in args:
-        p = os.path.join(p, elm)
-    return p
+pastLogs = []
 
+file = os.path.join(PATH, file)
+
+os.system("cls" if os.name == "nt" else "clear")
+while 1:
+    if os.path.exists(file):
+        logs = []
+        with open(file, "r", encoding="UTF-8") as f:
+            for line in f.readlines():
+                logs.append(line)
+            logs = logs[len(pastLogs):]
+            for l in logs:
+                print(l.strip())
+            pastLogs += logs
+    else:
+        print("File not found")
+        time.sleep(2)
+        sys.exit(1)
+    time.sleep(0.1)
